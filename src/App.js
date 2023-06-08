38360f257e238d99bbe98f2d17c12ccedee944bd
Ayush Raj <ayush.raj138@gmail.com>
2023-06-08 22:41:03 +0530

    Initialize repository with basic structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {useEffect }from "react";
+import axios from "axios";
+import Mockman from "mockman-js";
 function App() {
+  const getUser=async (user,pass)=>
+  {
+    try{
+      const response=await axios.post("/api/auth/login",{username:user,password:pass})
+      console.log(response)
+    }
+    catch (e){
+      console.log(e);
+    }
+  }
+//   useEffect(()=>{
+//     (async()=>{
+//     try {const response=await axios.post("/api/auth/login",{
+//       username: "adarshbalika",
+//     password: "adarshBalika123"
+//   })
+//     console.log(response)
+// }
+//     catch (e)
+//     {
+//       console.log(e)
+//     }
+// })()
+//   },[])
+getUser("adarshbalika","adarshBalika123")
   return (
     <div className="App">
       <header className="App-header">
+        {/* <Mockman/> */}
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
